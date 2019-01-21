7b7994933075c0dc81874a2324039364d886c08e
ryanarj <karshan813@gmail.com>
2019-01-21 15:45:48 -0500

    Error handling and middleware

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ let express = require('express')
 let app = express()
  
 let personRoute = require('./routes/person')
+
+let path = require('path')
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
+
 app.use(personRoute)
 app.use(express.static('public'))
+
+// Handler for 404 - not found
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost')
+})
+
+//Handler for error 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
--- src/routes/person.js
+++ src/routes/person.js
@@ -6,12 +6,16 @@ router.get('/person', (req, res) => {
     if(req.query.name){
         res.send(`You have requested a person ${req.query.name}`)
     } else {
-        res.send('You have requested a person ')
+        res.send('You have requested a person')
     }
 })
 
 // localhost:3000/person/ryan
 router.get('/person/:name', (req, res) => {
     res.send(`You have requested a person ${req.params.name}`)
+})
+
+router.get('/error', (req, res) => {
+    throw new Error('Forced error')
 })
 module.exports = router
